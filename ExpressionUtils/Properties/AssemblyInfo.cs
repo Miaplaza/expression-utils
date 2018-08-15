49b5055d281426187694a63467cb53c9bc1e5315
Julian Rüth <julian.rueth@fsfe.org>
2018-08-15 21:57:03 +0200

    Use semver and update assembly version
    fixes #16

--- ExpressionUtils/Properties/AssemblyInfo.cs
+++ ExpressionUtils/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MiaPlaza")]
 [assembly: AssemblyProduct("MiaPlaza.ExpressionUtils.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.2")]
