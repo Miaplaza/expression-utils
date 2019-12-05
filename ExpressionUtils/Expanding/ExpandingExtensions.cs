5d39108d877b8ce8c9b12f1acf962a9b926e9b4c
Bohdan Shchur <bogdanshur1@gmail.com>
2019-12-05 16:41:01 +0200

    Add other overloads(up to 16 arguments) for Expression Eval method

--- ExpressionUtils/Expanding/ExpandingExtensions.cs
+++ ExpressionUtils/Expanding/ExpandingExtensions.cs
@@ -116,6 +116,42 @@ namespace MiaPlaza.ExpressionUtils.Expanding {
 		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7)
 			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7);
 
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10, P11 p11)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10, P11 p11, P12 p12)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10, P11 p11, P12 p12, P13 p13)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10, P11 p11, P12 p12, P13 p13, P14 p14)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10, P11 p11, P12 p12, P13 p13, P14 p14, P15 p15)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15);
+
+		[ExpanderTypeExpressionExpandableMethod(typeof(SubExpressionExpander))]
+		public static R Eval<R, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16>(this Expression<Func<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, R>> expression, P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7, P8 p8, P9 p9, P10 p10, P11 p11, P12 p12, P13 p13, P14 p14, P15 p15, P16 p16)
+			=> (R)evaluator.EvaluateLambda(expression).Invoke(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16);
+
 		/// <summary>
 		/// Rewrites a call to any of the 'Eval' methods above by inlining the 
 		/// expression that would have been evaluated.
